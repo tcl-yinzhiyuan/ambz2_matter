0a04ab6725868480e3fe4ccd918ac9cf055613bf
step0035 <step0035@e.ntu.edu.sg>
2023-01-20 15:52:06 +0800

    move back orignal light

--- component/common/application/matter/core/matter_attribute.cpp
+++ component/common/application/matter/core/matter_attribute.cpp
@@ -1,11 +1,28 @@
 #include "matter_attribute.h"
+#include "matter_flags.h"
+#include "chip_porting.h"
 
 Attribute::Attribute(ameba_matter_attr_val_t attribute_value, uint32_t _attribute_id, uint16_t attribute_flags)
 {
     // TODO: add bounds, kvs
     val = attribute_value;
     attribute_id = _attribute_id;
-    flags = attribute_flags;
+    flags = attribute_flags | ATTRIBUTE_FLAG_EXTERNAL_STORAGE;
+
+    /* store non-volatile attributes in kvs */
+    if (flags & ATTRIBUTE_FLAG_NONVOLATILE)
+    {
+        // setup key (endpoint_id/cluster_id/attribute_id)
+        // char *key = (char *) pvPortMalloc(VARIABLE_NAME_SIZE);
+        // setPref_new
+        // attempt to get from kvs
+        // if get from kvs success, set the attribute_value to the value gotten from kvs
+        // else, set the attribute_value to the value passed in, then store in kvs
+    }
+    else
+    {
+        // set the attribute_value to the value passed in, don't need to store in kvs
+    }
 }
 
 Attribute *Attribute::get_next()
